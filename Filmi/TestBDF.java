0735bacb59cef313492b5a836edee6fad95a3a5e
FeriCodeDummy <94200904+FeriCodeDummy@users.noreply.github.com>
2022-04-17 19:50:06 +0200

    Update and rename Tester.java to Filmi/TestBDF.java

--- Filmi/TestBDF.java
+++ Filmi/TestBDF.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Tester {
+public class TestBDF {
 
 
 	public static void main(String[] args) throws IOException {
